58f1c333c32efc5b39790e8d68c443948a5714f7
Sagar Jagtap <sagar.jagtap@neosoftmail.com>
2023-05-31 14:31:50 +0530

    init

---
+++ server.js
@@ -0,0 +1,41 @@
+const grpc = require("@grpc/grpc-js");
+const PROTO_PATH = "./news.proto";
+var protoLoader = require("@grpc/proto-loader");
+
+const options = {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true,
+};
+var packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
+const newsProto = grpc.loadPackageDefinition(packageDefinition);
+
+const server = new grpc.Server();
+// let news = [
+//   { id: "1", title: "Note 1", body: "Content 1", postImage: "Post image 1" },
+//   { id: "2", title: "Note 2", body: "Content 2", postImage: "Post image 2" },
+// ];
+
+let news = [
+  { id: "1" },
+  { id: "2" },
+];
+
+server.addService(newsProto.NewsService.service, {
+  GetAllNews: (requestBody, callback) => {
+      console.log(news)
+     callback(null, news);
+  },
+});
+
+server.bindAsync(
+  "127.0.0.1:50051",
+  grpc.ServerCredentials.createInsecure(),
+  (error, port) => {
+    console.log("Server at port:", port);
+    console.log("Server running at http://127.0.0.1:50051");
+    server.start();
+  }
+);
